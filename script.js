8a13f4a6a3aa789435b7ee18384efa417b960b44
Dennis Schroder <57254955+Dennis-Github@users.noreply.github.com>
2021-09-08 10:02:09 +0000

    Game

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var x, y;
+let x = 320;
+let y = 180;
+let xspeed = 5;
+let yspeed = 12;
+
+let r = 25;
 
 function setup() {
-	createCanvas(500, 400);
-
-  x = 0;
-  y = 200;
+  createCanvas(640, 360);
 }
 
 function draw() {
-	background(225);
-  
-  ellipse(x,y,50,50);
-  x = x + 5;
-
-  if(x > 500){
-    x = 0;
+  background(225);
+  ellipse(x, y, r*2, r*2);
+  x += xspeed;
+  y += yspeed;
+  if (x > width - r || x < r) {
+    xspeed = -xspeed;
   }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+
+
 }
