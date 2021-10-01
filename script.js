3c615c3db5ee4cd203cdbfbed462201cd4161934
Dennis Schroder <57254955+Dennis-Github@users.noreply.github.com>
2021-10-01 09:57:58 +0000

    3 balletjes gemaakt die vallen op verschillende snelheden en op verschillende plekken beginnen + die van buiten het scherm starten

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ class Ball{
 
     this.y = this.y + this.vy;
 
-     if(this.y < 0 || this.y > 400){
+     if(this.y < -100 || this.y > 400){
       this.y = 400;
     }
   }
 }
 
-var ball, ball1;
+var ball, ball1, ball2;
 
 function setup() {
   createCanvas(400, 400);
 
-  ball = new Ball(10, 0,5);
-  ball1 = new Ball(200, 0,3);
+  ball = new Ball(10, -20,2);
+  ball1 = new Ball(200, -80,3);
+  ball2 = new Ball(300,-60,1)
   
 }
 
@@ -32,4 +33,5 @@ function draw() {
 
     ball.drawBall();
     ball1.drawBall();
+    ball2.drawBall();
 }
