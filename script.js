df67f8a11b1e8aab0767af37489171429fb45ed4
Dennis Schroder <57254955+Dennis-Github@users.noreply.github.com>
2021-10-01 10:05:16 +0000

    Nu maar 1 keer drawball ipv elke keer opnieuw

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 
-class Ball{
-  constructor(x,y,vy){
+class Ball {
+  constructor(x, y, vy) {
     this.x = x;
     this.y = y;
     this.vy = vy;
   }
 
-  drawBall(){
-    ellipse(this.x,this.y,20,20);
+  drawBall() {
+    ellipse(this.x, this.y, 20, 20);
 
     this.y = this.y + this.vy;
 
-     if(this.y < -100 || this.y > 400){
+    if (this.y < -100 || this.y > 400) {
       this.y = 400;
     }
   }
 }
 
-var ball, ball1, ball2;
+var balls = [];
 
 function setup() {
   createCanvas(400, 400);
 
-  ball = new Ball(10, -20,2);
-  ball1 = new Ball(200, -80,3);
-  ball2 = new Ball(300,-60,1)
-  
+  ball = new Ball(10, -20, 2);
+  ball1 = new Ball(200, -80, 3);
+  ball2 = new Ball(300, -60, 1);
+  balls.push(ball);
+  balls.push(ball1);
+  balls.push(ball2);
+
 }
 
 function draw() {
-    background(220);
+  background(220);
 
-    ball.drawBall();
-    ball1.drawBall();
-    ball2.drawBall();
+  balls.forEach((b) => {
+    b.drawBall();
+  })
 }
