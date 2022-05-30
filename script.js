92b188e4f7e8529df7a34a195b71e21afc02b39b
Dennis Schroder <57254955+Dennis-Github@users.noreply.github.com>
2022-05-30 12:42:04 +0000

    Gamestate start, play en end. (Menu's)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let score = 0;
 class Ball {
   constructor(vy) {
     this.x = random(width);
@@ -37,42 +36,64 @@ class Ball {
   }
 }
 
-
+var score = 0;
 var balls = [];
 var song = [];
+var gameState = 'start';
 
 function preload() {
   song = loadSound("Rainbow.mp3");    
 }
 
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400); 
 }
 
 function draw() {
   background(220);
-  fill("orange")
-  rect(mouseX - 70, 350, 140, 30)
-  text("Score: " + score, 10, 20); 
+  fill("orange")   
+  
+  if (gameState === 'start') {
+    fill('purple');
+    textAlign (CENTER);
+    text('Press space to play', width / 2, height / 2);
+  }
 
-
-  if (frameCount % 80 == 0) {
-    balls.push(new Ball(random(6)));
+  if (gameState == "play"){
+    game();
   }  
+ 
+  if (gameState === 'end') {
+    text('game over, press down arrow to end', width / 2, height / 2);
+  }    
+}
+
+  
+
+function game(){
+  text('You are playing the game now, press down arrow to stop', width / 2, 100);
+  text("Score: " + score, 10, 20);    
+   rect(mouseX - 70, 350, 140, 30)
+   if (frameCount % 80 == 0) {
+      balls.push(new Ball(random(6)));
+    }  
 
   balls.forEach((b) => {
     b.drawBall();
     b.checkCollision();
-  });    
+  });
 }
 
-function keyPressed() {
-  console.log("keyCode", keyCode)
-  if(keyCode == 32){
-    song.play();
-  }
-  else{
+function keyPressed() {  
+  if (keyCode === 40){
+    gameState = 'end';
     song.stop();
   }
+    
+  if(keyCode == 32){
+    gameState = "play"
+    //song.play();
+  }
 }
-  
